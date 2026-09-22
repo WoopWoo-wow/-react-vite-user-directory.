e90235215a1b9688fab9fc297dbb2d84141a2314
andytaguba-ctrl <andytaguba@g.cjc.edu.ph>
2026-09-22 16:50:57 +0800

    newpythontopics

---
+++ newtopics/arraylistvsnumpy.py
@@ -0,0 +1,7 @@
+import numpy as sixseven
+from tkinter import simpledialog, messagebox
+
+numbers = sixseven.array([121, 242, 312, 443, 155])
+
+messagebox.showinfo("Result", f"The result is: {numbers * 2 * 2}")
+
